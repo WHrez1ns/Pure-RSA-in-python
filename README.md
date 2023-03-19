7c84eb37990b4ed9f449fe93ebca799146cde49c
Renan <renan.dias0871@gmail.com>
2023-03-19 11:43:08 -0300

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Pure-RSA-in-python
- A python script trying to replicate the behavior of RSA public key encryption
+# Algoritmo de RSA PURO em python
+ Um script python tentando replicar o comportamento da criptografia de chave pública RSA
+
+# Depurando o código
+ O algoritmo implementado utiliza o RSA para criptografar e descriptografar uma mensagem, seguindo os passos definidos na descrição do problema.
+
+ O código começa importando as bibliotecas necessárias: *random*, *sympy* e *gcd* do módulo *math*.
+
+ O passo 1 é implementado em um *loop while* que gera dois números primos aleatórios. Isso é feito utilizando a função *randint()* da biblioteca *random*, que gera um número aleatório entre os limites definidos. Para verificar se o número é primo, é utilizada a função *isprime()* da biblioteca *sympy*.
+
+ Em seguida, o código define a função *gerar_chaves()*, que *implementa os passos 2, 3, 4 e 5 do algoritmo*. Essa função recebe como parâmetros os números primos *p* e *q* e retorna as chaves *pública* e *privada*.
+
+ Dentro da função, primeiro é calculado *N (pq)* e *Z ((p-1)(q-1))*. Em seguida, um número aleatório e coprimo com *Z* é escolhido utilizando a função *randrange()* da biblioteca *random* e um *loop while* que verifica *se o maior divisor comum entre E e Z é igual a 1*.
+
+ Depois, é calculado o *inverso multiplicativo de E em relação a Z* utilizando a função *inverso_modular()*, que implementa o *algoritmo de Euclides estendido*.
+
+ Por fim, as chaves *pública* e *privada* são retornadas em uma *tupla*.
+
+ A função *criptografar()* recebe uma mensagem e a *chave pública* e retorna a mensagem *criptografada*. Para isso, ela *converte cada caractere da mensagem* para o seu *código ASCII*, *eleva esse valor à potência E* e faz o *módulo N*. O resultado é *armazenado* em uma *lista* e essa *lista* é retornada.
+
+ A função *descriptografar()* recebe a mensagem *criptografada* e a *chave privada* e retorna a *mensagem original*. Para isso, ela *eleva cada valor da lista criptograma à potência D* e faz o *módulo N*. Em seguida, *converte o resultado para o caractere correspondente* no *código ASCII* e *junta* todos os *caracteres* em uma *única string*.
+
+ A função *inverso_modular()* *verifica* se *A* e *M* *são coprimos*, ou seja, *se o maior divisor comum entre eles é 1*. Se isso *não for verdadeiro*, a função retorna *None*. Caso contrário, ela utiliza o *algoritmo de Euclides estendido* para *encontrar* o *inverso multiplicativo de A em relação a M*. O resultado é retornado fazendo o *módulo M*.
+
+ O *algoritmo de Euclides estendido* é implementado na função *euclides_estendido()*. Essa função *recebe dois números* *A* e *B* e *retorna* o *maior divisor comum entre eles* e os valores *X* e *Y* que *satisfazem* a equação *AX + BY = gcd(A,B)*. O algoritmo é *implementado* de *forma recursiva*.
+
+ # Por fim 
+ O código *chama as funções necessárias para gerar as chaves*, *criptografar* e *descriptografar* a *mensagem* e *imprimir* os *resultados* na tela. A *mensagem original*, a *mensagem criptografada* e a *mensagem decifrada* são impressas na tela para *verificação*.
+
+# Considerações finais
+ Nunca trabalhei em algo tão insano quanto isso - por mim :D
---
+++ script.py
@@ -0,0 +1,114 @@
+##########################################################################################
+# Construa  o  algoritmo com  a  linguagem  de  programação  de  sua  preferência,  ele  #
+# deve escolher dois valores aleatórios para p e q, geraras chaves pública e privada de  #
+# acordo com os quatropassos apresentados. O algoritmo deve ser capaz de criptografar e  #
+# decriptografar a frase “The information security is of significant importanceto ensure #
+# the privacy of communications”.                                                        #
+##########################################################################################
+
+# Passo 1 - Escolher de forma aleatória dois números primos grandes (p q)
+# Passo 2 - Calcular N => N = p.q
+# Passo 3 - Encontre o valor Z => m.m.c de p-1*q-1
+# Passo 4 - Encontrar o valor de E => E > 1 < Z
+# Passo 5 - Definir D => inverso multiplicativo de E
+# Chaves Públicas: N e E
+# Chaves Privadas: P, Q e D
+# Encriptar => (textoplano^E) % N
+# Decriptar => (textoplano^D) % N
+
+import random  # importando a biblioteca Random para gerar números aleatórios
+import sympy  # importando a biblioteca Sympy para trabalhar com matemática simbólica
+# importando o gcd do math (Greatest Common Divisor) para realizar o cálculo de maior divisor comum
+from math import gcd
+
+# Passo 1
+
+# Essa é a função que gera as chaves pública e privada. Ela recebe dois números primos p e q, calcula n como o produto de p e q e z como o produto de p-1 e q-1.
+
+
+def gerar_chaves(p, q):
+    n = p * q
+    z = (p - 1) * (q - 1)
+
+    # Escolher um número aleatório e coprime com Z
+    e = random.randrange(1, z)
+    while gcd(e, z) != 1:
+        e = random.randrange(1, z)
+
+    # Calcular o inverso multiplicativo de E em relação a Z
+    d = inverso_modular(e, z)
+
+    # Retornar as chaves pública e privada
+    return ((n, e), (n, d))
+
+
+# Função Criptografar
+
+
+def criptografar(mensagem, chave_publica):
+    n, e = chave_publica
+    criptograma = [(ord(char) ** e) % n for char in mensagem]
+    return criptograma
+
+
+# Função Decriptografar
+
+
+def descriptografar(criptograma, chave_privada):
+    n, d = chave_privada
+    mensagem = [chr((char ** d) % n) for char in criptograma]
+    return ''.join(mensagem)
+
+
+# Calculo do inverso modular
+
+
+def inverso_modular(a, m):
+    # Verificar se a e m são coprimos
+    if gcd(a, m) != 1:
+        return None
+
+    # Encontrar o inverso multiplicativo de A em relação a M utilizando o algoritmo de Euclides estendido
+    _, x, _ = euclides_estendido(a, m)
+    return x % m
+
+
+# Algoritmo de euclides estendido
+
+
+def euclides_estendido(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = euclides_estendido(b % a, a)
+        return gcd, y - (b // a) * x, x
+
+
+lista_primos = []
+
+print("Gerando N primos...")
+while len(lista_primos) < 2:
+    primo = 0
+    while not sympy.isprime(primo):
+        # obs: o correto seria elevado a 6 e 7 para gerarem numeros maiores, mas o meu processador é bem do ruim então para rodar eu diminui :D
+        primo = random.randint(10**2, 10**3)
+    lista_primos.append(primo)
+
+print("Calculando...")
+p = lista_primos[0]
+q = lista_primos[1]
+mensagem = 'The information security is of significant importanceto ensure the privacy of communications'
+chave_publica, chave_privada = gerar_chaves(p, q)
+print("Gerando...")
+criptograma = criptografar(mensagem, chave_publica)
+print("Criptografia...")
+mensagem_decifrada = descriptografar(criptograma, chave_privada)
+print("Decriptografia...")
+
+print("---------------------------------------------------------------------------------------------------------")
+print("Mensagem original:", mensagem)
+print("---------------------------------------------------------------------------------------------------------")
+print("Mensagem criptografada:", criptograma)
+print("---------------------------------------------------------------------------------------------------------")
+print("Mensagem decifrada:", mensagem_decifrada)
+print("---------------------------------------------------------------------------------------------------------")
